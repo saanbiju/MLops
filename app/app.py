47ad476f322e9fac091073e00d2ff6a2d3e692e4
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2025-07-26 21:34:05 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -13,7 +13,7 @@ except Exception as e:
 
 @app.route("/")
 def index():
-    return "Welcome to the Flask ML App  narewshIT🎉"
+    return "Welcome to the Flask ML App  narewshIT-2🎉"
 
 @app.route("/predict", methods=["GET"])
 def predict():
